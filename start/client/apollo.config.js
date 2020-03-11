5f37f66698c3a025401030f951cfe648da771607
Wang Wenlin <sopl.wang@gmail.com>
2020-03-12 03:50:31 +0800

    Kameo: initial import

---
+++ start/client/apollo.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  client: {
+    includes: ['./src/*/**/*.{ts,tsx}'],
+    service: {
+      url: 'http://localhost:4000/graphql',
+      name: 'fullstack-tutorial',
+    },
+  }
+};
